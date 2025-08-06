e12a972eeeda0133fd10eb9d8477a9f5c8124ba4
student-p <paulmoon00@naver.com>
2025-08-06 10:24:28 +0900

    init

---
+++ src/main/java/hackathon/mindbullet/MindbulletApplication.java
@@ -0,0 +1,13 @@
+package hackathon.mindbullet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindbulletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindbulletApplication.class, args);
+    }
+
+}
