e04f8867b35c79eb41c721b2eb4bb5cca08150f6
student-p <paulmoon00@naver.com>
2025-08-07 01:24:23 +0900

    feat: 메모 수정 기능구현

--- src/main/java/hackathon/mindbullet/modules/memo/api/MemoController.java
+++ src/main/java/hackathon/mindbullet/modules/memo/api/MemoController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +31,11 @@ public class MemoController {
     public ResponseEntity<Void> createMemo(@RequestBody MemoRequest memoRequest) {
         memoService.createMemo(memoRequest);
         return ResponseEntity.status(CREATED).build();
+    }
+
+    @PutMapping("/{memoId}")
+    public ResponseEntity<Void> updateMemo(@PathVariable Long memoId, @RequestBody MemoRequest memoRequest) {
+        memoService.updateMemo(memoId, memoRequest);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/hackathon/mindbullet/modules/memo/exception/MemoExceptionType.java
+++ src/main/java/hackathon/mindbullet/modules/memo/exception/MemoExceptionType.java
@@ -4,7 +4,8 @@ import hackathon.mindbullet.golbal.base.BaseExceptionType;
 import org.springframework.http.HttpStatus;
 
 public enum MemoExceptionType implements BaseExceptionType {
-    NOT_EXISTS_ID(HttpStatus.NOT_FOUND, "존재하지 않는 메모 id입니다.")
+    NOT_EXISTS_ID(HttpStatus.NOT_FOUND, "존재하지 않는 메모 id입니다."),
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.")
     ;
 
     private final HttpStatus httpStatus;
