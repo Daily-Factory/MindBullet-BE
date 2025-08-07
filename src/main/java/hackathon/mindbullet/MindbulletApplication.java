664f8848c35273dbd970310c230580424bb84851
student-p <paulmoon00@naver.com>
2025-08-07 11:41:45 +0900

    feat: JPA Auditing 활성화

--- src/main/java/hackathon/mindbullet/MindbulletApplication.java
+++ src/main/java/hackathon/mindbullet/MindbulletApplication.java
@@ -2,8 +2,10 @@ package hackathon.mindbullet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MindbulletApplication {
 
     public static void main(String[] args) {
