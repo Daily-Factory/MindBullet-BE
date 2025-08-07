b764e3611ac2459e369577b78f72bc10fed09435
student-p <paulmoon00@naver.com>
2025-08-07 13:55:10 +0900

    feat: h2 데이터베이스 사용과 초기 데이터 생성 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO board (id, date) VALUES (1, NOW());
+
+INSERT INTO memo (id, title, type, content, password, board_id) VALUES (1, 'Meeting Notes', 'NOTE', 'Discuss project
+timeline and resources', '1111', 1);
+INSERT INTO memo (id, title, type, content, password, board_id) VALUES (2, 'Task Reminder', 'TASK', 'Complete report
+by EOD', '1111', 1);
